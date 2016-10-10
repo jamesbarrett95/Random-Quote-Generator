1a8495d78e4b2bde15bc4bcb66039a909213ae47
BBD\james.barrett <james.barrett@brightblueday.com>
2016-10-10 17:26:14 +0100

    Tweet Quote Functionality 90 Percent Complete, HTML to be stripped

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+var quote;
+
 $("#newquote").click(function(){
   $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=30&jsonp=mycallback", function(a) {
     $(".quotearea").empty();
     $(".quotearea").addClass("border");
-    var quote = a[Math.floor(Math.random()*a.length)];
+    quote = a[Math.floor(Math.random()*a.length)];
     $(".quotearea").append(quote.content + " " + quote.title);
+
+    $('.tweetquote:hidden').show();
   });
+});
+
+$('.tweetquote').click(function() {
+  window.open('https://twitter.com/intent/tweet?text=' + quote.content + quote.title);
 });
